9f36b61f898b74596f1858b1b553fb8fd986e692
kendam <kenny@ksolutionsng.com>
2022-08-27 19:55:01 +0100

    Created binary search and a sample illustration

---
+++ divide_and_conquer/binary_search.js
@@ -0,0 +1,37 @@
+// basic sorting 
+
+
+/**
+ * returns the index of target value in a sorted array using o(logn) if found otherwise returns -1
+ * @param {[]} sortedArray  soreted array
+ * @param {Number} value  target value
+ * @returns {Number}
+ */
+function binarySearch(sortedArray,value){
+    //get the first index
+    let firstIndex  = 0;
+
+    //get the last index
+    let lastIndex = sortedArray.length - 1
+
+    while(firstIndex <= lastIndex){
+        // get the middleIndex
+        let middleIndex = Math.floor((firstIndex +lastIndex)/2);
+
+        // compare the target value with the mid value
+        if(sortedArray[middleIndex] < value){
+            firstIndex  =  middleIndex + 1;
+        }
+        else if (sortedArray[middleIndex] > value){
+            lastIndex = middleIndex - 1;
+        }
+        else {
+            return middleIndex;
+        }
+    }
+
+    return -1;
+
+}
+let  sortedArray = [21,24,26,38,40,44]
+console.log(binarySearch(sortedArray,10));
---
+++ divide_and_conquer/binary_search_problem_1.js
@@ -0,0 +1,64 @@
+/*
+A teacher instructs his trainees to queue in ascending order of their scores.
+An Interesting pattern was a common difference in the scores. 
+When he compared his record with the queue he realised one of the trainee was no where to be found  Write an Algorithm to locate the lacking trainee
+
+Assumptions:  
+ 1. Given that the misplaced score is between the  item in the last  and third item from the end;
+ 2. There will be at least 6 known scores
+
+Expected output:  The missing score is <score> found at position <position>
+
+where <score> and <position> are placeholders
+
+*/
+
+function locator(scores){
+    let firstIndex  = 1;
+    let lastIndex = scores.length - 3
+
+    while(firstIndex <= lastIndex){
+       
+        //find the middel index
+        let middleIndex = Math.floor((firstIndex +lastIndex)/2);
+
+
+        // calculate the common difference
+        let commonDifference = scores[1] - scores[0];
+
+        // Compare the difference between the middle score and the succeding score with the common difference
+        if(scores[middleIndex + 1] - scores[middleIndex] !== commonDifference  ){
+            return  scores[middleIndex + 1  ] -  commonDifference
+
+        }
+
+        // Compare the difference between the middle score and the preceeding score with the common difference
+        if(scores[middleIndex] - scores[middleIndex - 1] !== commonDifference  ){
+            return  scores[middleIndex - 1  ] +  commonDifference
+
+        }
+
+        // using the formular for the last term of an arithmetic progression => L = a + (n-1)d
+        // Check if the middle score matches its actual value
+        // A match implies the missing score is from numbers greater than the middle score
+        // else it 
+
+        if(scores[middleIndex] !== (scores[firstIndex] + ((middleIndex-1)* commonDifference))){
+            
+           
+            lastIndex = middleIndex - 1;
+           
+        }
+        else {
+            firstIndex  =  middleIndex + 1;
+        }
+       
+    }
+
+    return -1;
+
+}
+
+let  sortedArray = [45,50,55,60,65,75,80,85,90,95]
+//console.log(sortedArray)
+console.log('missing number is ', locator(sortedArray));
